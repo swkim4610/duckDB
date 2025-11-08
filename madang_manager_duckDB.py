ad33dc4f1eab0c04374e69459da31c5c0a5d73e5
swkim4610 <swkim4610@gmail.com>
2025-11-08 10:47:47 +0900

    Update madang_manager_duckDB.py

--- madang_manager_duckDB.py
+++ madang_manager_duckDB.py
@@ -1,27 +1,29 @@
 import streamlit as st 
 import duckdb
 import pandas as pd
-import time
+from datetime import datetime
+from pytz import timezone
 
 dbConn = duckdb.connect('madang.db')
 cursor = dbConn.cursor()
+seoul = timezone('Asia/Seoul')
 
 def query(sql):
-       return dbConn.execute(sql).fetchall()
+       return dbConn.execute(sql).df()
 
 st.title("마당 서점 관리 시스템")
 
-books = [None]
-result = query("select concat(bookid, '. ', bookname) from Book")
-for res in result:
-       books.append(res[0])
+customers = query("select name from Customer;")
+result = query("select concat(bookid, '. ', bookname) as bookname from Book")
+books = result['bookname'].tolist()
 
 tab1, tab2 = st.tabs(["고객조회", "거래 입력"])
 name = ""
+select_book = ""
 custid = 999
 result =pd.DataFrame()
-name = tab1.text_input("고객명")
-select_book = ""
+
+name = tab1.selectbox("고객명", customers)
 
 if len(name) > 0:
        sql = "select c.custid, c.name, b.bookname, o.orderdate, o.saleprice from Customer c, Book b, Orders o \
@@ -35,9 +37,9 @@ if len(name) > 0:
 
        if select_book is not None:
               bookid = select_book.split(".")[0]
-              dt = time.localtime()
-              dt = time.strftime('%Y-%m-%d', dt)
-              orderid = query("select max(orderid) from orders;")[0][0] + 1
+              now = datetime.now(seoul)
+              dt = now.strftime('%Y-%m-%d')
+              orderid = query("select max(orderid) as val from orders;")['val'][0] + 1
               price = tab2.text_input("금액")
               sql = "insert into orders (orderid, custid, bookid, saleprice, orderdate) values (" \
                   + str(orderid) + ","  \
@@ -48,4 +50,5 @@ if len(name) > 0:
               if tab2.button('거래 입력'):
                      cursor.execute(sql)
                      dbConn.commit()
+                     dbConn.close()
                      tab2.write('거래가 입력되었습니다.')
